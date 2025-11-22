7e21ab8b627d3349f6180edf84e9cced1e2fea80
PTOM76 <ptms76@outlook.jp>
2025-11-22 14:39:38 +0900

    feat: improve menu

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,54 @@ use crossterm::{
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode}
 };
 
+const OPTIONS: [&str; 6] = ["vim", "fmtui", "btop", "nmtui", "", "quit"];
+
+/// main
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let options = OPTIONS;
+    let mut choice = 0;
+
+    loop {
+        let mut stdout = stdout();
+
+        execute!(stdout, EnterAlternateScreen, Hide)?;
+        enable_raw_mode()?;
+
+        let backend = CrosstermBackend::new(&mut stdout);
+        let mut terminal = Terminal::new(backend)?;
+
+        loop {
+            draw(&mut terminal, &options, choice)?;
+            if input(options.len(), &mut choice)? {
+                break;
+            }
+        }
+
+        disable_raw_mode()?;
+        drop(terminal);
+        execute!(stdout, LeaveAlternateScreen, Show)?;
+
+        match options[choice] {
+            "vim" => { std::process::Command::new("vim").status()?; }
+            "fmtui" => { std::process::Command::new("fmtui").status()?; }
+            "btop" => { std::process::Command::new("btop").status()?; }
+            "nmtui" => { std::process::Command::new("nmtui").status()?; }
+            "quit" => { break; }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+
 /// draw
 ///
 /// # Arguments
 /// * `terminal` - Reference to terminal object
 /// * `options` - Slice of option strings to display
-/// * `choice` - Current selected index
+/// * `choice` - choice index
 ///
 /// # Returns
 /// is successful?
@@ -51,71 +93,33 @@ fn draw(terminal: &mut Terminal<CrosstermBackend<&mut io::Stdout>>, options: &[&
 ///
 /// # Arguments
 /// * `options_len` - Length of options
-/// * `current` - Mutable reference to current selected index
+/// * `choice` - Mutable reference to choice index
 //// # Returns
 /// is exit flag?
-fn input(options_len: usize, current: &mut usize) -> Result<bool> {
+fn input(options_len: usize, choice: &mut usize) -> Result<bool> {
     if event::poll(std::time::Duration::from_millis(200))? {
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Up => *current = if *current == 0 { options_len - 1 } else { *current - 1 },
-                KeyCode::Down => *current = if *current == options_len - 1 { 0 } else { *current + 1 },
+                KeyCode::Up => {
+                    loop {
+                        *choice = if *choice == 0 { options_len - 1 } else { *choice - 1 };
+                        if !OPTIONS[*choice].is_empty() {
+                            break;
+                        }
+                    }
+                },
+                KeyCode::Down => {
+                    loop {
+                        *choice = if *choice == options_len - 1 { 0 } else { *choice + 1 };
+                        if !OPTIONS[*choice].is_empty() {
+                            break;
+                        }
+                    }
+                }            
                 KeyCode::Enter | KeyCode::Esc => return Ok(true),
                 _ => {}
             }
         }
     }
     Ok(false)
-}
-
-/// main
-fn main() -> Result<()> {
-    color_eyre::install()?;
-
-    let options = vec!["vim", "fmtui", "btop", "nmtui", "quit"];
-    let mut choice = 0;
-
-    loop {
-        let mut stdout = stdout();
-
-        execute!(stdout, EnterAlternateScreen, Hide)?;
-        enable_raw_mode()?;
-
-        let backend = CrosstermBackend::new(&mut stdout);
-        let mut terminal = Terminal::new(backend)?;
-
-        loop {
-            draw(&mut terminal, &options, choice)?;
-            if input(options.len(), &mut choice)? {
-                break;
-            }
-        }
-
-        disable_raw_mode()?;
-
-        drop(terminal);
-
-        execute!(stdout, LeaveAlternateScreen, Show)?;
-
-        match options[choice] {
-            "vim" => {
-                std::process::Command::new("vim").status()?;
-            }
-            "fmtui" => {
-                std::process::Command::new("fmtui").status()?;
-            }
-            "btop" => {
-                std::process::Command::new("btop").status()?;
-            }
-            "nmtui" => {
-                std::process::Command::new("nmtui").status()?;
-            }
-            "quit" => {
-                break;
-            }
-            _ => {}
-        }
-    }
-
-    Ok(())
 }
